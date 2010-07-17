72a0d62ad98dde67712b2ddae1c19ea078fff0f7
David A Roberts <d@vidr.cc>
2010-07-17 17:07:25 +1000

    add mfc7400c post

--- jekyll/_plugins/filters.rb
+++ jekyll/_plugins/filters.rb
@@ -3,5 +3,9 @@ module Jekyll
         def normalize_url(url)
             url.sub(/index\.html$/, "")
         end
+
+        def summarize_post(content)
+            content.sub(/<h.*/m, "<p><em>Continued after the jump...</em></p>")
+        end
     end
 end
